cde9e1cbac79c9dad03206f58091989ffb9a48b6
otavius <developer.otavius@gmail.com>
2026-08-02 12:41:14 -0400

    working on backtesting function

--- main.py
+++ main.py
@@ -27,11 +27,33 @@ def zscore(symbol: str, timeframe = mt5.TIMEFRAME_H1, window: int = 20, date_fro
     rates_df["rolling_mean"]= rates_df["close"].rolling(window=window).mean() 
     rates_df["rolling_std"] = rates_df["close"].rolling(window=window).std()
     rates_df["zscore"] = (rates_df["close"] - rates_df["rolling_mean"]) / rates_df["rolling_std"]
+    print(rates_df.columns)
 
 
     return rates_df.dropna()
 
+def backtest(df, entry_z: float = 2.0, exit_z: float = 0.5, stop_z: float = 3.5):
+    position = 0 # 0 = flat, 1 = long, 2 = short
+    entry_price = None
+    trades = []
 
+    for i in range(len(df) -1):
+        current = df.iloc[i]
+        next_bar = df.iloc[i + 1]
+        if position == 0:
+            if current["zscore"] > entry_z:
+                position = 2
+                entry_price = next_bar["open"] 
+            elif current["zscore"] < -entry_z:
+                position = 1
+                entry_price = next_bar["open"]
+        elif position == 1:
+            pass 
+        elif position == 2: 
+            pass 
+
+    
+    return trades
 
 if __name__ == "__main__":
     print(zscore("GBPJPY", mt5.TIMEFRAME_H1, window=20))
