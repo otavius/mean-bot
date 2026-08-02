d6e8a874c2946327b21197081931fc5234c74493
otavius <developer.otavius@gmail.com>
2026-08-02 19:19:05 -0400

    finish working on backtesting function

--- main.py
+++ main.py
@@ -43,18 +43,58 @@ def backtest(df, entry_z: float = 2.0, exit_z: float = 0.5, stop_z: float = 3.5)
         if position == 0:
             if current["zscore"] > entry_z:
                 position = 2
-                entry_price = next_bar["open"] 
+                entry_price = next_bar["open"]
             elif current["zscore"] < -entry_z:
                 position = 1
                 entry_price = next_bar["open"]
-        elif position == 1:
-            pass 
-        elif position == 2: 
-            pass 
+        elif position != 0:
+            if abs(current["zscore"]) >= stop_z:
+                # close position at loss
+                if position == 1: 
+                    exit_price = next_bar["open"]
+                    pnl = exit_price - entry_price
+                elif position == 2:
+                    exit_price = next_bar["open"]
+                    pnl = entry_price - exit_price
 
-    
+
+                trades.append(dict({
+                    "entry_price": entry_price,
+                    "exit_price":exit_price, 
+                    "pnl": pnl,
+                    "position": position,
+                    "reason": "stop out"
+                }))
+                position = 0
+                entry_price = None
+            elif abs(current["zscore"]) < exit_z:
+                if position == 1: 
+                    exit_price = next_bar["open"]
+                    pnl = exit_price - entry_price
+                elif position == 2:
+                    exit_price = next_bar["open"]
+                    pnl = entry_price -  exit_price
+
+
+                trades.append(dict({
+                    "entry_price": entry_price,
+                    "exit_price":exit_price, 
+                    "pnl": pnl,
+                    "position": position,
+                    "reason": "exit trade"
+                }))
+                position = 0 
+                entry_price = None
+
     return trades
 
 if __name__ == "__main__":
-    print(zscore("GBPJPY", mt5.TIMEFRAME_H1, window=20))
+    #print(zscore("GBPJPY", mt5.TIMEFRAME_H1, window=20))
+    data = zscore("EURUSD", mt5.TIMEFRAME_H1, window=20)
+    trades = backtest(data)
+    reasons = [t["reason"] for t in trades]
+    print(f"stop out = {reasons.count("stop out")}, exits = {reasons.count("exit trade")}")
+    print(len(trades))
+    print(trades[:5])
+
     mt5.shutdown()
